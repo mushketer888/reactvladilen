949a09bc2ff95b27c5a00254124b2c695a3e2ac6
Ilja <mushketer888@gmail.com>
2020-09-17 21:37:02 +0300

    Init

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import TodoList from './Todo/TodoList'
+import Context from './context'
+import AddTodo from './Todo/AddTodo'
+
+function App() {
+  const [todos,setTodos] = React.useState([
+    {id:1, completed:false, title: 'Kupitj mozgi'},
+    {id:2, completed:false, title: 'Kupitj pivo'},
+    {id:3, completed:false, title: 'Kupitj vodku'}
+  ])
+
+
+function toggleTodo(id){
+  console.log('todo id',id)
+  setTodos(todos.map(todo=>{
+    if (todo.id === id) {
+      todo.completed = !todo.completed
+    }
+    return todo
+  })
+  )
+}
+
+function removeTodo(id){
+  setTodos(todos.filter(todo=>todo.id!==id))
+}
+
+function addTodo(title){
+  setTodos(todos.concat([{
+    title,
+    id: Date.now(),
+    completed: false
+  }]))
+}
+
+  return (
+    <Context.Provider value={{removeTodo}}>
+    <div className="wrapper">
+      <h1>React tutorial</h1>
+      <AddTodo onCreate={addTodo}/>
+      {todos.length?<TodoList todos={todos} onToggle={toggleTodo} />:<p>No todos!</p>}
+      
+    </div>
+    </Context.Provider>
+  )
+}
+
+export default App;
